622bf1e1068882ecaaa39a91270eb7020b458927
Ziv <zivlazarov@gmail.com>
2023-01-20 19:35:45 +0200

    added error printing for current error codes which are stored in constants.h file (to be filled)

--- assembler/constants.h
+++ assembler/constants.h
@@ -1,1 +1,9 @@
+#define MEMORY_NOT_ALLOCATED_ERROR_CODE -1
+#define HASH_TABLE_INSERT_CONTAINS_KEY_ERROR_CODE -2
+#define HASH_TABLE_KEY_DOESNT_EXIST_ERROR_CODE -3
 
+
+#define MACRO_ALREADY_EXISTS_ERROR_MESSAGE "This macro already exists. Exiting...\n"
+#define HASH_TABLE_KEY_ALREADY_EXIST_ERROR_MESSAGE "This key already exists in the hash table.\n"
+#define HASH_TABLE_KEY_DOESNT_EXIST_ERROR_MESSAGE "This key doesn't exist in the hash table.\n"
+#define FILE_NOT_EXIST_ERROR_MESSAGE "The file you specified doesn't exist..\n"
