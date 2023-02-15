180ab8ffcc2f9a61992ba2507c92d26e41613f0a
lazarovziv <zivlazarov@gmail.com>
2023-02-15 16:46:58 +0200

    fixed comments prevernting from macro to be deployed properly. assuming comments are written at beginning of the line and not after a regular command.

--- assembler/pre_assembler.c
+++ assembler/pre_assembler.c
@@ -203,6 +203,12 @@ void read_macros_from_file(FILE *file, hashTable *table) {
     free(word);
     free(macroName);
     free(macroBody);
+
+    for (j = 0; j < table->size; j++) {
+        if (table->items[j] != NULL) {
+            printf("key: %s\n", table->items[j]->key);
+        }
+    }
 }
 
 void write_macros_to_file(FILE *readFile, FILE *writeFile, hashTable *table) {
@@ -217,6 +223,7 @@ void write_macros_to_file(FILE *readFile, FILE *writeFile, hashTable *table) {
         j = 0;
         k = strlen(word);
         while (isspace(word[j])) j++;
+        if (word[j] == ';') continue;
         if (j == k) continue;
 
         while (isspace(word[k])) k--;
