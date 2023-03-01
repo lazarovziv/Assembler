6bd31687cbea0fb719777df45320fdcb7aa00dfc
Ziv <zivlazarov@gmail.com>
2023-03-02 01:14:02 +0200

    added header file for pre_assembler for testing and setting constants

--- assembler/pre_assembler.c
+++ assembler/pre_assembler.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "hash_table.h"
-#include "constants.h"
-
-#define MAX_WORD_LENGTH 81
-#define HASH_TABLE_SIZE 100
-
-#define READ_MODE "r"
-#define WRITE_MODE "w"
-#define MACRO_KEYWORD "mcr"
-#define END_MACRO_KEYWORD "endmcr"
-
-enum macroState {
-    NOT_IN_MACRO, IN_MACRO_BODY, IN_MACRO_NAME, END_MACRO
-};
-
-int read_macros_from_file(FILE *file, hashTable *table);
-int write_macros_to_file(FILE *readFile, FILE *writeFile, hashTable *table);
-int deploy_macros(int argc, char *argv[]);
+#include "pre_assembler.h"
 
 int main(int argc, char *argv[]) {
     if (!deploy_macros(argc, argv)) return 0;
@@ -235,7 +215,7 @@ int read_macros_from_file(FILE *file, hashTable *table) {
                 break;
             }
 
-                /* default case should be IN_MACRO_NAME which is handled in NOT_IN_MACRO case while loop */
+            /* default case should be IN_MACRO_NAME which is handled in NOT_IN_MACRO case while loop */
             default: {
                 break;
             }
