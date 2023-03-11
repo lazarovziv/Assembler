6fbed6b8969d0b847e8bcf93fd5db213b8ed7e3c
lazarovziv <zivlazarov@gmail.com>
2023-03-11 19:20:31 +0200

    added function to convert decimal to special binary. handled .data/string encoding. removed unneessary functions

--- assembler/encoder.c
+++ assembler/encoder.c
@@ -1,49 +1,60 @@
 #include "encoder.h"
-#include "validate_file.h"
 
 const char *REGISTERS_NAMES[] = {
         "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8" };
 
-int first_scan(FILE *file, hashTable *table, int *IC, int *DC) {
-    int i, j;
+int first_scan(FILE *file, FILE *writeFile, hashTableInt *table, int *IC, int *DC) {
+    int i, j, startParamsIdx;
     /* flag whether to encode currentLine of not */
     int encodeLine = 1;
     /* counting starts from 1 */
-    int lineNum = 0, lineNumTemp;
-    /* for counting num of digits */
-    int lineNumDigitCounter, maxLineNumDigitCounter = -1;
+    int lineNum = 0;
     /* reading line from file */
     char currentLine[MAX_WORD_LENGTH];
+    char copyCurrentLine[MAX_WORD_LENGTH];
+    char *token;
     /* label to store in symbol's table (as key) */
     char *labelName = NULL;
     char storeDataString[STRING_STORE_COMMAND_LENGTH]; /* strlen(".string") = 7 */
-    /* label's row in file to store in symbol's table (as value)  */
-    char *labelRow = NULL;
     int currentLabelLength, maxLabelLength = -1;
-    int currentLabelCommandCode;
     int insertLabelReturnCode;
-
-    /* addressing type for each parameter */
-    enum addressingType firstAddressingType, secondAddressingType;
-
+    int numRegistersInCurrentLine = 0, firstRegisterIdx, secondRegisterIdx;
+    /* for iterating through linked list in hash table incrementing each node by IC */
+    hashTableIntItem *current;
     /* current line number of words */
     int L;
+    /* encoded string */
+    char encodedString[NUM_BITS];
+
+    short encodedWord = 0;
+    short operationEncode, firstParamEncode, secondParamEncode, srcEncode, destEncode, externEntryEncode;
+    operationEncode = firstParamEncode = secondParamEncode = srcEncode = destEncode = externEntryEncode = 0;
 
     while (fgets(currentLine, MAX_WORD_LENGTH, file) != NULL) {
-        L = validLine(currentLine);
-        printf("L: %d\n", L);
+        L = 0;
+        strcpy(copyCurrentLine, currentLine);
+        copyCurrentLine[strlen(copyCurrentLine)] = '\0';
+        L = validLine(copyCurrentLine);
         lineNum++;
-        lineNumDigitCounter = 0;
         currentLabelLength = 0;
 
         if (!L) {
             /* TODO: print error */
-            printf("invalid!\n");
+            printf("Line %d invalid!\n", lineNum);
+            encodeLine = 0;
+            continue;
+        }
+
+        if (!encodeLine) {
+            encodeLine = 1;
             continue;
         }
 
         /* no label declared */
-        /*if (!strstr(currentLine, ":")) continue;*/
+        if (!strstr(currentLine, ":")) {
+            /* TODO: handle "regular" commands */
+            continue;
+        }
 
         for (i = 0; isspace(currentLine[i]); i++);
         /* if a label is declared */
@@ -65,6 +76,7 @@ int first_scan(FILE *file, hashTable *table, int *IC, int *DC) {
         }
         /* initializing labelName */
         strncpy(labelName, &currentLine[i-currentLabelLength], currentLabelLength);
+        labelName[currentLabelLength] = '\0';
 
         /* skipping whitespaces after colons */
         /*for (i = i+1; isspace(currentLine[i]); i++);*/
@@ -74,215 +86,152 @@ int first_scan(FILE *file, hashTable *table, int *IC, int *DC) {
 
         /* .data or .string */
         if (currentLine[i] == '.') {
-            storeDataString[0] = '.';
             /* setting storeDataString */
-            for (j = i+1; isalpha(currentLine[j]); j++) {
-                storeDataString[j - i] = currentLine[j];
+            for (j = i; currentLine[j] != ' '; j++) {
+                storeDataString[j-i] = currentLine[j];
             }
+            storeDataString[j-i] = '\0';
 
             /* insert label into hash table with DC value as value */
-            /* counting digits in DC */
-            lineNumTemp = *DC;
-            while (lineNumTemp != 0) {
-                lineNumDigitCounter++;
-                lineNumTemp /= 10;
-            }
-
-            if (labelRow == NULL) {
-                labelRow = (char *) malloc(lineNumDigitCounter);
-                maxLineNumDigitCounter = lineNumDigitCounter;
-            } else if (maxLineNumDigitCounter < lineNumDigitCounter) {
-                maxLineNumDigitCounter = lineNumDigitCounter;
-                labelRow = (char *) realloc(labelRow, maxLineNumDigitCounter);
-            }
-
-            if (labelRow == NULL) {
-                fprintf(stderr, MEMORY_NOT_ALLOCATED_SUCCESSFULLY_ERROR_MESSAGE);
-                return MEMORY_NOT_ALLOCATED_ERROR_CODE;
-            }
-
-            /* converting line number to string for storing in hash table */
-            lineNumTemp = *DC;
-            for (i = lineNumDigitCounter - 1; i >= 0; i--) {
-                labelRow[i] = ((char) (lineNumTemp % 10)) + '0';
-                lineNumTemp /= 10;
-            }
-
-            /* inserting labels into symbols table */
-            insertLabelReturnCode = insert(table, labelName, labelRow);
+            insertLabelReturnCode = insert_int(table, labelName, *DC);
 
             if (insertLabelReturnCode == HASH_TABLE_INSERT_CONTAINS_KEY_ERROR_CODE) {
                 fprintf(stderr, "Label %s already exists!\n", labelName);
                 free(labelName);
-                free(labelRow);
                 return -1; /* TODO: handle errors if label already exists */
             }
 
             /* .data declaration */
             if (strcmp(storeDataString, DATA_STORE_COMMAND) == 0) {
-                /* skipping space between .data and first argument and counting commas */
-                for (j = j + 1; currentLine[j] != '\0'; j++) {
-                    /* TODO: encode numbers */
-                    if (currentLine[j] == ',') (*DC)++;
+                startParamsIdx = j+1;
+                token = strtok(&copyCurrentLine[startParamsIdx], ", \n");
+                while (token != NULL) {
+                    encodedWord = (short) atoi(token);
+                    encodedWord = encodedWord << 2;
+                    convert_to_special_binary(encodedWord, encodedString);
+                    fprintf(writeFile, "%s\n", encodedString);
+                    (*DC)++;
+                    token = strtok(NULL, ", \n");
                 }
-                /* number of arguments = number of commas + 1 */
-                (*DC)++;
+
                 /* .string declaration */
             } else if (strcmp(storeDataString, STRING_STORE_COMMAND) == 0) {
-                /* skipping space between .string and '\"' and counting characters until '\"' */
-                for (j = j + 2; currentLine[j] != '\"'; j++) {
-                    /* TODO: encode characters */
+                startParamsIdx = j + 2;
+                /* skipping space between .string and '"' and counting characters until '"' */
+                for (j = startParamsIdx; currentLine[j] != '"'; j++) {
+                    encodedWord = (short) currentLine[j];
+                    encodedWord = encodedWord << 2;
+                    convert_to_special_binary(encodedWord, encodedString);
+                    fprintf(writeFile, "%s\n", encodedString);
                     (*DC)++;
                 }
+                convert_to_special_binary(0, encodedString);
+                /* writing \0 value at the end */
+                fprintf(writeFile, "%s\n", encodedString);
             }
 
-            (*IC) += L;
-
             memset(labelName, 0, maxLabelLength);
-            memset(labelRow, 0, lineNumDigitCounter);
             memset(currentLine, 0, MAX_WORD_LENGTH);
-            memset(storeDataString, 0, 7);
+            memset(copyCurrentLine, 0, MAX_WORD_LENGTH);
+            memset(storeDataString, 0, STRING_STORE_COMMAND_LENGTH);
+            continue;
+            /* not storing data */
+        } else {
+            insertLabelReturnCode = insert_int(table, labelName, *IC);
+
+            if (insertLabelReturnCode == HASH_TABLE_INSERT_CONTAINS_KEY_ERROR_CODE) {
+                fprintf(stderr, "Label %s already exists!\n", labelName);
+                free(labelName);
+                return -1; /* TODO: handle errors if label already exists */
+            }
+
+            for (startParamsIdx = 0; currentLine[startParamsIdx] != ':'; startParamsIdx++);
+            startParamsIdx++;
+            strcpy(copyCurrentLine, currentLine);
+            token = strtok(&copyCurrentLine[startParamsIdx], " \t\n,");
+            operationEncode = command_code(token);
+
+            while (token != NULL) {
+                if (command_code(token) == -1) {
+                    if (numRegistersInCurrentLine == 0) {
+                        if ((firstRegisterIdx = is_register(token))) {
+                            numRegistersInCurrentLine++;
+                            srcEncode = firstRegisterIdx;
+                        }
+                    } else if (numRegistersInCurrentLine == 1) {
+                        if ((secondRegisterIdx = is_register(token))) {
+                            numRegistersInCurrentLine++;
+                            destEncode = secondRegisterIdx;
+                        }
+                    }
+                }
+                token = strtok(NULL, " \t\n,");
+            }
+
+            /* can be mov, cmp, add, sub */
+            if (numRegistersInCurrentLine == 2) {
+                /* writing operation code in first line and registers in one line */
+                encodedWord = (firstRegisterIdx << SHIFTS_FOR_FIRST_REGISTER) | (secondRegisterIdx << SHIFTS_FOR_SECOND_REGISTER);
+                convert_to_special_binary((operationEncode << SHIFTS_FOR_OPCODE), encodedString);
+                fprintf(writeFile, "%s\n", encodedString);
+                convert_to_special_binary(encodedWord, encodedString);
+                fprintf(writeFile, "%s\n", encodedString);
+            }
+
+            firstRegisterIdx = secondRegisterIdx = 0;
+
+            (*IC) += L;
             continue;
         }
 
         /* not a declaration of a .data or .string (incrementing IC) can be a regular command (i.e: LOOP: mov #2, r4) */
-        currentLabelCommandCode = command_code(&currentLine[i]);
 
         /* TODO: handle extern/entry commands */
 
         memset(labelName, 0, maxLabelLength);
-        memset(labelRow, 0, lineNumDigitCounter);
         memset(currentLine, 0, MAX_WORD_LENGTH);
+        memset(copyCurrentLine, 0, MAX_WORD_LENGTH);
 
         (*IC) += L;
     }
 
+    (*DC) += *IC;
+
+    /* incrementing all symbol's table values by IC */
+    for (i = 0; i < table->size; i++) {
+        if (table->items[i]) {
+            current = table->items[i];
+            while (current) {
+                current->value += *IC;
+                current = current->next;
+            }
+        }
+    }
+
     free(labelName);
-    free(labelRow);
+
     return 1;
 }
 
-int encode_mov_command(char *command, enum addressingType *first, enum addressingType *second) {
-    int i = -1;
-    int tempI;
-    float tempF;
-    /* index in command where each parameter starts */
-    int firstParamIdx, secondParamIdx;
-    /* registers' number */
-    int firstRegisterIdx = -1, secondRegisterIdx = -1;
-    /* for storing parameters' values */
-    void *firstValue = NULL, *secondValue = NULL;
-    /* identifying addressing type with first symbol (./#/etc.) */
-    char firstParamSymbol, secondParamSymbol;
-    /* each parameter's substring */
-    char *firstParamString = NULL, *secondParamString = NULL;
-    int isFirstFloat = 0;
+void convert_to_special_binary(int num, char finalString[]) {
+    int i;
+    /* mask = 0010000000000000 */
+    short mask = 1 << (NUM_BITS-1);
 
-    firstParamIdx = strlen("mov ");
-    firstParamSymbol = command[firstParamIdx];
-    /* immediate addressing */
-    if (firstParamSymbol == '#') {
-        *first = IMMEDIATE;
-
-        /* traversing all characters before comma */
-        for (i = firstParamIdx+1; command[i] != ','; i++);
-        /* creating first parameter string */
-        firstParamString = (char *) malloc(i - firstParamIdx - 1);
-
-        if (firstParamString == NULL) {
-            fprintf(stderr, MEMORY_NOT_ALLOCATED_SUCCESSFULLY_ERROR_MESSAGE);
-            return MEMORY_NOT_ALLOCATED_ERROR_CODE;
-        }
-
-        for (i = firstParamIdx+1; command[i] != ','; i++) {
-            if (command[i] == '.') isFirstFloat = 1;
-            firstParamString[i-firstParamIdx-1] = command[i];
-        }
-
-        if (isFirstFloat) {
-            tempF = (float) atof(firstParamString);
-            firstValue = &tempF;
-        } else {
-            tempI = atoi(firstParamString);
-            firstValue = &tempI;
-        }
-        /* register addressing */
-    } else if (firstParamSymbol == 'r' && strlen(firstParamString) == 2) {
-        /* traversing all characters before comma */
-        for (i = firstParamIdx; command[i] != ','; i++);
-        /* creating first parameter string */
-        firstParamString = (char *) malloc(i - firstParamIdx - 1);
-
-        if (firstParamString == NULL) {
-            fprintf(stderr, MEMORY_NOT_ALLOCATED_SUCCESSFULLY_ERROR_MESSAGE);
-            return MEMORY_NOT_ALLOCATED_ERROR_CODE;
-        }
-        /* first param is a register (register addressing) */
-        for (i = firstParamIdx; command[i] != ','; i++) {
-            firstParamString[i-firstParamIdx] = command[i];
-        }
-        if ((firstRegisterIdx = is_register(firstParamString))) {
-            *first = REGISTER;
-        }
-    } else {
-
+    for (i = 0; i < NUM_BITS; i++) {
+        if (num & mask) finalString[i] = '/';
+        else finalString[i] = '.';
+        mask = mask >> 1;
     }
-
-    /* checking second parameter */
-    i++;
-    for (; isspace(command[i]); i++);
-    secondParamIdx = i;
-    secondParamSymbol = command[secondParamIdx];
-
-    if (secondParamSymbol == '.') {
-        *second = DIRECT;
-
-        /* traversing all characters before comma */
-        for (i = secondParamIdx+1; command[i] != ','; i++);
-        /* creating first parameter string */
-        secondParamString = (char *) malloc(i - secondParamIdx - 1);
-
-        if (secondParamString == NULL) {
-            fprintf(stderr, MEMORY_NOT_ALLOCATED_SUCCESSFULLY_ERROR_MESSAGE);
-            return MEMORY_NOT_ALLOCATED_ERROR_CODE;
-        }
-        /* first param is a label (direct addressing) */
-        for (i = secondParamIdx+1; command[i] != ','; i++) {
-            secondParamString[i-secondParamIdx-1] = command[i];
-        }
-    } else if (secondParamSymbol == 'r' && strlen(secondParamString) == 2) {
-        /* checking for register addressing */
-        if ((secondRegisterIdx = is_register(secondParamString))) {
-            *second = REGISTER;
-        }
-    }
-
-    return 0;
+    finalString[NUM_BITS] = '\0';
 }
-
-/*
-int encode_cmp_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_add_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_sub_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_not_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_clr_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_lea_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_inc_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_dec_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_jmp_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_bne_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_red_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_prn_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_jsr_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_rts_command(char *command, enum addressingType *first, enum addressingType *second) {}
-int encode_stop_command(char *command, enum addressingType *first, enum addressingType *second) {}*/
 
 int command_code(char *command) {
     int i, spaceIdx;
     char *commandSubstring;
-    int commandCode = UNKNOWN_COMMAND_CODE;
+    int commandCode = UNKNOWN_CODE;
 
-    if (strcmp(command, "stop") == 0) return STOP_COMMAND_CODE;
+    if (strcmp(command, "stop") == 0) return STOP_CODE;
 
     for (spaceIdx = 0; command[spaceIdx] != ' ' && command[spaceIdx] != '\t'; spaceIdx++);
     commandSubstring = (char *) malloc(spaceIdx);
@@ -294,21 +243,21 @@ int command_code(char *command) {
 
     for (i = 0; i < spaceIdx; i++) commandSubstring[i] = command[i];
 
-    if (strcmp(commandSubstring, "mov") == 0) commandCode = MOV_COMMAND_CODE;
-    if (strcmp(commandSubstring, "cmp") == 0) commandCode = CMP_COMMAND_CODE;
-    if (strcmp(commandSubstring, "add") == 0) commandCode = ADD_COMMAND_CODE;
-    if (strcmp(commandSubstring, "sub") == 0) commandCode = SUB_COMMAND_CODE;
-    if (strcmp(commandSubstring, "not") == 0) commandCode = NOT_COMMAND_CODE;
-    if (strcmp(commandSubstring, "clr") == 0) commandCode = CLR_COMMAND_CODE;
-    if (strcmp(commandSubstring, "lea") == 0) commandCode = LEA_COMMAND_CODE;
-    if (strcmp(commandSubstring, "inc") == 0) commandCode = INC_COMMAND_CODE;
-    if (strcmp(commandSubstring, "dec") == 0) commandCode = DEC_COMMAND_CODE;
-    if (strcmp(commandSubstring, "jmp") == 0) commandCode = JMP_COMMAND_CODE;
-    if (strcmp(commandSubstring, "bne") == 0) commandCode = BNE_COMMAND_CODE;
-    if (strcmp(commandSubstring, "red") == 0) commandCode = RED_COMMAND_CODE;
-    if (strcmp(commandSubstring, "prn") == 0) commandCode = PRN_COMMAND_CODE;
-    if (strcmp(commandSubstring, "jsr") == 0) commandCode = JSR_COMMAND_CODE;
-    if (strcmp(commandSubstring, "rts") == 0) commandCode = RTS_COMMAND_CODE;
+    if (strcmp(commandSubstring, "mov") == 0) commandCode = MOV_CODE;
+    if (strcmp(commandSubstring, "cmp") == 0) commandCode = CMP_CODE;
+    if (strcmp(commandSubstring, "add") == 0) commandCode = ADD_CODE;
+    if (strcmp(commandSubstring, "sub") == 0) commandCode = SUB_CODE;
+    if (strcmp(commandSubstring, "not") == 0) commandCode = NOT_CODE;
+    if (strcmp(commandSubstring, "clr") == 0) commandCode = CLR_CODE;
+    if (strcmp(commandSubstring, "lea") == 0) commandCode = LEA_CODE;
+    if (strcmp(commandSubstring, "inc") == 0) commandCode = INC_CODE;
+    if (strcmp(commandSubstring, "dec") == 0) commandCode = DEC_CODE;
+    if (strcmp(commandSubstring, "jmp") == 0) commandCode = JMP_CODE;
+    if (strcmp(commandSubstring, "bne") == 0) commandCode = BNE_CODE;
+    if (strcmp(commandSubstring, "red") == 0) commandCode = RED_CODE;
+    if (strcmp(commandSubstring, "prn") == 0) commandCode = PRN_CODE;
+    if (strcmp(commandSubstring, "jsr") == 0) commandCode = JSR_CODE;
+    if (strcmp(commandSubstring, "rts") == 0) commandCode = RTS_CODE;
 
     free(commandSubstring);
     return commandCode;
--- assembler/encoder.h
+++ assembler/encoder.h
@@ -1,31 +1,13 @@
 #include <string.h>
 #include "hash_table.h"
+#include "validate_file.h"
 
 enum addressingType {
     IMMEDIATE, DIRECT, JUMP, REGISTER
 };
 
-int first_scan(FILE *file, hashTable *table, int *IC, int *DC);
+int first_scan(FILE *file, FILE *writeFile, hashTableInt *table, int *IC, int *DC);
 int command_code(char *command);
-int encode_mov_command(char *command, enum addressingType *first, enum addressingType *second);
-/*
-int encode_cmp_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_add_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_sub_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_not_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_clr_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_lea_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_inc_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_dec_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_jmp_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_bne_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_red_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_prn_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_jsr_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_rts_command(char *command, enum addressingType *first, enum addressingType *second);
-int encode_stop_command(char *command, enum addressingType *first, enum addressingType *second);
- */
-
-void encode_command(char *command, int commandCode);
-
 int is_register(char *string);
+
+void convert_to_special_binary(int num, char finalString[]);
